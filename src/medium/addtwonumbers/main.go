5749032e7af85b71b6c06858d00cf1f74ea820e4
pestzzz <zhuguo_2007@163.com>
2020-04-21 15:44:17 +0800

    修改逻辑 ... 额 总算过了。。。

--- src/medium/addtwonumbers/main.go
+++ src/medium/addtwonumbers/main.go
@@ -12,7 +12,7 @@ func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
 	result := &ListNode{}
 	var lastNode *ListNode
 
-	for l1 != nil || l2 != nil {
+	for l1 != nil || l2 != nil || val/10 != 0 {
 		val1 := 0
 		val2 := 0
 
@@ -41,7 +41,7 @@ func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
 }
 
 func main() {
-	l1 := &ListNode{Val: 2, Next: &ListNode{Val: 4, Next: &ListNode{Val: 3}}}
+	l1 := &ListNode{Val: 5, Next: &ListNode{Val: 4, Next: &ListNode{Val: 3}}}
 	l2 := &ListNode{Val: 5, Next: &ListNode{Val: 6, Next: &ListNode{Val: 4}}}
 
 	result := addTwoNumbers(l1, l2)
