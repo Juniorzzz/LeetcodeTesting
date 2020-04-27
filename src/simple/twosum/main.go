74301c983d0eb28d128afa2f8bd8bee78fba724c
pestz <zhuguoqiang@bjb.com.cn>
2020-04-27 11:45:45 +0800

    添加题目

--- src/simple/twosum/main.go
+++ src/simple/twosum/main.go
@@ -1,34 +1,35 @@
 package main
 
 /***
+两数之和
 给定一个整数数组 怒貌似和一个目标值 target，请你在数组中找出和为目标值的两个整数，并返回他们的数组下标
 你可以根据假设每种输入只会对应一个答案。但是，数组中同一个元素不能使用两遍。
 示例：
 	给定nums =[2,7,11,15], target=9
 	因为nums[0]+num[1]=2+7=9
 	所以返回[0,1]
- */
+*/
 
-func TwoSum(nums []int, target int ) (bool, []int){
+func TwoSum(nums []int, target int) (bool, []int) {
 	v := make(map[int]int)
-	for i:=0;i<len(nums);i++ {
+	for i := 0; i < len(nums); i++ {
 		if value, has := v[target-nums[i]]; has {
-			return true, []int{i,value}
+			return true, []int{i, value}
 		}
-		v[nums[i]]=i
+		v[nums[i]] = i
 	}
 	return false, nil
 }
 
-func main(){
+func main() {
 
-	nums := []int{ 1,2,3,45,12}
+	nums := []int{1, 2, 3, 45, 12}
 	ret, val := TwoSum(nums, 13)
 
 	if ret {
 		print("result:")
 		print(val[0], val[1])
-	}else {
+	} else {
 		print("result: null")
 	}
 
