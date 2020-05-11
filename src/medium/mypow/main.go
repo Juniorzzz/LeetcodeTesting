44ec302030408eeafff4fbe321240391535d7827
pestz <zhuguoqiang@bjb.com.cn>
2020-05-11 10:09:30 +0800

    添加题目

---
+++ src/medium/mypow/main.go
@@ -0,0 +1,52 @@
+/****
+50. Pow(x, n)
+实现 pow(x, n) ，即计算 x 的 n 次幂函数。
+
+示例 1:
+
+输入: 2.00000, 10
+输出: 1024.00000
+示例 2:
+
+输入: 2.10000, 3
+输出: 9.26100
+示例 3:
+
+输入: 2.00000, -2
+输出: 0.25000
+解释: 2-2 = 1/22 = 1/4 = 0.25
+说明:
+
+-100.0 < x < 100.0
+n 是 32 位有符号整数，其数值范围是 [−231, 231 − 1] 。
+*/
+package main
+
+func calc(x float64, n int) float64 {
+
+	ret := 1.0
+	val := x
+
+	for n > 0 {
+		if n%2 == 1 {
+			ret *= val
+		}
+
+		val *= val
+		n /= 2
+	}
+	return ret
+}
+
+func myPow(x float64, n int) float64 {
+
+	if n < 0 {
+		return 1.0 / calc(x, -n)
+	}
+
+	return calc(x, n)
+}
+
+func main() {
+
+}
